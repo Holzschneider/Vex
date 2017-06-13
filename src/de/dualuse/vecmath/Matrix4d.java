d20138efc5d669bbed8cf8dbb7dd2e91a0aebe1f
Philipp Holzschneider <philipp@covicon.de>
2017-06-13 20:37:32 +0200

    Matrix4d.toString() fix

--- src/de/dualuse/vecmath/Matrix4d.java
+++ src/de/dualuse/vecmath/Matrix4d.java
@@ -186,9 +186,9 @@ public class Matrix4d extends Matrix<Matrix4d> implements Serializable {
 	
 	
 	@Override public String toString() {
-		return	m00+" "+m01+" "+m02+" "+m03+
-				m10+" "+m11+" "+m12+" "+m13+
-				m20+" "+m21+" "+m22+" "+m23+
+		return	m00+" "+m01+" "+m02+" "+m03+" "+
+				m10+" "+m11+" "+m12+" "+m13+" "+
+				m20+" "+m21+" "+m22+" "+m23+" "+
 				m30+" "+m31+" "+m32+" "+m33;
 	}
 
